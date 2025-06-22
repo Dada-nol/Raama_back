03ecc5180d56cbe67c5ca3fe198decc8bbdd7f97
Darren <nardol.darren@gmail.com>
2025-06-22 17:32:39 +0200

    Add UserController

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class UserController extends Controller
+{
+  public function register(Request $request)
+  {
+    $request->validate([
+      'name' => 'required',
+      'email' => 'required|email|unique:users',
+      'password' => 'required|confirmed'
+    ]);
+
+    $user = User::create([
+      'name' => $request->name,
+      'email' => $request->email,
+      'password' => bcrypt($request->password)
+    ]);
+
+    return response()->json($user, 201);
+  }
+
+  public function login(Request $request)
+  {
+    $request->validate([
+      'email' => 'required|email',
+      'password' => 'required'
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+      throw ValidationException::withMessages([
+        'email' => ['Les informations sont invalides.'],
+      ]);
+    }
+
+    return [
+      'token' => $user->createToken('mobile')->plainTextToken
+    ];
+  }
+
+  public function logout(Request $request)
+  {
+    $request->user()->currentAccessToken()->delete();
+
+    return response()->json(['message' => 'Déconnexion réussie']);
+  }
+
+  public function user(Request $request)
+  {
+    return $request->user();
+  }
+
+  public function delete(Request $request)
+  {
+    $user = $request->user();
+
+    if (! $user) {
+      throw ValidationException::withMessages([
+        'email' => ['Les informations sont invalides.'],
+      ]);
+    }
+    $user->delete();
+
+    return response()->json(['message' => 'Suppression réussie']);
+  }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,67 +4,15 @@
  * Auth Routes
  */
 
-use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Validation\ValidationException;
 
-Route::post('/register', function (Request $request) {
-  $request->validate([
-    'name' => 'required',
-    'email' => 'required|email|unique:users',
-    'password' => 'required|confirmed'
-  ]);
+Route::post('/register', [UserController::class, "register"]);
 
-  $user = User::create([
-    'name' => $request->name,
-    'email' => $request->email,
-    'password' => bcrypt($request->password)
-  ]);
+Route::post('/login', [UserController::class, "login"]);
 
-  return response()->json($user, 201);
-});
+Route::middleware('auth:sanctum')->post('/logout', [UserController::class, "logout"]);
 
-Route::post('/login', function (Request $request) {
-  $request->validate([
-    'email' => 'required|email',
-    'password' => 'required'
-  ]);
+Route::middleware('auth:sanctum')->get('/user', [UserController::class, "user"]);
 
-  $user = User::where('email', $request->email)->first();
-
-  if (! $user || ! Hash::check($request->password, $user->password)) {
-    throw ValidationException::withMessages([
-      'email' => ['Les informations sont invalides.'],
-    ]);
-  }
-
-  return [
-    'token' => $user->createToken('mobile')->plainTextToken
-  ];
-});
-
-Route::middleware('auth:sanctum')->post('/logout', function (Request $request) {
-  $request->user()->currentAccessToken()->delete();
-
-  return response()->json(['message' => 'Déconnexion réussie']);
-});
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-  return $request->user();
-});
-
-Route::middleware('auth:sanctum')->delete('/delete', function (Request $request) {
-  $user = $request->user();
-
-  if (! $user) {
-    throw ValidationException::withMessages([
-      'email' => ['Les informations sont invalides.'],
-    ]);
-  }
-  $user->delete();
-
-  return response()->json(['message' => 'Suppression réussie']);
-});
+Route::middleware('auth:sanctum')->delete('/delete', [UserController::class, "delete"]);
